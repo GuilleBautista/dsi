ba379ea30268eba42dfd40201bf42e7c6401e70b
akua21 <albareinders@gmail.com>
2020-02-17 15:07:03 +0100

    nuse

---
+++ src-jhcProyecto/src/app/alumnos/alumno.model.ts
@@ -0,0 +1,12 @@
+export interface Alumno {
+  id: number;
+  nia: string;
+  nombre: string;
+  apellidos: string;
+  grupo: number;
+}
+
+export interface Grupo {
+  id: number;
+  nombre: string;
+}
