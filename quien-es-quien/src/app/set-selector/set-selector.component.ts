7cc4fbd8b8f6e958781f7dd86be6fa9c74961f73
Guillermo Bautista <100383477@alumnos.uc3m.es>
2020-04-24 17:20:54 +0200

    first set is in db

--- quien-es-quien/src/app/set-selector/set-selector.component.ts
+++ quien-es-quien/src/app/set-selector/set-selector.component.ts
@@ -20,51 +20,50 @@ export class SetSelectorComponent implements OnInit {
   
   //Matriz para el tablero
   public Sets:Array<any>;
-
-  public setfb:Set[];
-  public s_sets:Subscription;
-
-  public ref:any;
   
-  public obsurl: Observable<string | null>;
+  
   public url:string;
 
   constructor(public router: Router, public route: ActivatedRoute, 
     private firestoreService: FirestoreService,private _snackBar: MatSnackBar, private storage:AngularFireStorage
     ) {
 
+    const ref = this.storage.ref('sets/1.png');
+    const obsurl = ref.getDownloadURL();
+
+
+    obsurl.subscribe(char=>{
+      this.url=char;
+    })
+
     this.Sets=[
       {
-        "route": 'assets/sets/1.png',
-        "id": 0
+        "route": "",
+        "id": "/characters/set0"
        },
        {
         "route": 'assets/sets/1.png (copy)', 
-        "id": 1
+        "id": "/characters/set1"
        },
        {
          "route": 'assets/sets/2.png',
-         "id": 2
+         "id": "/characters/set2"
        }
       ];
 
-      const ref = this.storage.ref('sets/set0/1.svg');
-      this.obsurl = ref.getDownloadURL();
-
-      const obssubs=this.obsurl.subscribe(char=>{
+      obsurl.subscribe(char=>{
         this.url=char;
-        console.log(this.url);
+        this.Sets[0].route=this.url;
       })
       
+     
   }
 
   ngOnInit(): void {
-    this.s_sets=this.firestoreService.getSets().subscribe(data=>{
-      this.setfb=data;
-    });
+    
   }
 
-  select(id:Number){
+  select(id:string){
     this.router.navigate(['/game'],     // En URL y participan en el routing.
     //{param_extra1:'XXX',param_extra2:57}],  // Se pasan codificados en la url.
     { state: { param_not_in_url: id }}   // No se muestran en la URL
